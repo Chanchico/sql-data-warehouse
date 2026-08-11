cb7c7ba061466c30cc8cf003ee47e98f8e208047
Chanchico <juanfranciscotraversi@gmail.com>
2026-08-11 12:25:27 +0200

    data exploration for silver layer

--- scripts/sandbox.sql
+++ scripts/sandbox.sql
@@ -1,1 +1,51 @@
-EXEC bronze.load_bronze
+SELECT TOP (1000) [cst_id],
+    [cst_key],
+    [cst_firstname],
+    [cst_marital_status],
+    [cst_gndr],
+    [cst_create_date]
+ FROM [DataWarehouse].[bronze].[crm_cust_info];
+
+SELECT TOP (1000) [sls_ord_num]
+      ,[sls_prd_key]
+      ,[sls_cust_id]
+      ,[sls_order_dt]
+      ,[sls_ship_dt]
+      ,[sls_due_dt]
+      ,[sls_sales]
+      ,[sls_quatity]
+      ,[sls_price]
+  FROM [DataWarehouse].[bronze].[crm_sales_details];
+
+  SELECT TOP (1000) [prd_id]
+      ,[prd_key]
+      ,[prd_nm]
+      ,[prd_cost]
+      ,[prd_line]
+      ,[prd_start_dt]
+      ,[prd_end_dt]
+  FROM [DataWarehouse].[bronze].[crm_prd_info];
+
+  SELECT * FROM bronze.crm_prd_info WHERE prd_key = 'HL-U509';
+
+SELECT TOP(1000) * FROM bronze.erp_cust_az12;
+SELECT TOP (1000) [cst_id],
+    [cst_key],
+    [cst_firstname],
+    [cst_marital_status],
+    [cst_gndr],
+    [cst_create_date]
+ FROM [DataWarehouse].[bronze].[crm_cust_info];
+
+
+ SELECT TOP 1000 * FROM bronze.erp_loc_a101
+
+ SELECT TOP 1000 * FROM bronze.erp_px_cat_g1v2;
+  SELECT TOP (1000) [prd_id]
+      ,[prd_key]
+      ,[prd_nm]
+      ,[prd_cost]
+      ,[prd_line]
+      ,[prd_start_dt]
+      ,[prd_end_dt]
+  FROM [DataWarehouse].[bronze].[crm_prd_info];
--- scripts/silver/dll.sql
+++ scripts/silver/dll.sql
@@ -3,18 +3,18 @@
 DDL Script: Create Bronze Tables
 =================================================================================
 Script Purpose:
-	This script creates tables  in the 'bronze' schemas, dropping existing table
+	This script creates tables  in the 'silver' schemas, dropping existing table
 	if they already exist
-Run this script to re-define the DDL structure of 'bronze' Tables 
+Run this script to re-define the DDL structure of 'silver' Tables 
 =================================================================================
 */
 
 
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_cust_info;
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_cust_info;
 GO 
 
-CREATE TABLE bronze.crm_cust_info(
+CREATE TABLE silver.crm_cust_info(
 	cst_id INT,
 	cst_key NVARCHAR(50),
 	cst_firstname NVARCHAR(50),
@@ -26,11 +26,11 @@ CREATE TABLE bronze.crm_cust_info(
 GO
 
 ----------------------------------------------------------
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_prd_info;
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+	DROP TABLE silver.crm_prd_info;
 GO
 
-CREATE TABLE bronze.crm_prd_info(
+CREATE TABLE silver.crm_prd_info(
 	prd_id INT,
 	prd_key NVARCHAR(50),
 	prd_nm NVARCHAR(50),
@@ -42,11 +42,11 @@ CREATE TABLE bronze.crm_prd_info(
 GO
 
 ----------------------------------------------------------
-If OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-	DROP TABLE bronze.crm_sales_details;
+If OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+	DROP TABLE silver.crm_sales_details;
 GO
 
-CREATE TABLE bronze.crm_sales_details(
+CREATE TABLE silver.crm_sales_details(
 	sls_ord_num NVARCHAR(50),
 	sls_prd_key NVARCHAR(50),
 	sls_cust_id INT,
@@ -60,11 +60,11 @@ CREATE TABLE bronze.crm_sales_details(
 GO
 
 ---------------------------------------------------------
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_cust_az12;
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+	DROP TABLE silver.erp_cust_az12;
 GO
 
-CREATE TABLE bronze.erp_cust_az12(
+CREATE TABLE silver.erp_cust_az12(
 	cid NVARCHAR(50),
 	bdate DATE,
 	gen NVARCHAR(50)
@@ -72,22 +72,22 @@ CREATE TABLE bronze.erp_cust_az12(
 GO
 
 -----------------------------------------------------------
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_loc_a101; 
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+	DROP TABLE silver.erp_loc_a101; 
 GO
 
-CREATE TABLE bronze.erp_loc_a101(
+CREATE TABLE silver.erp_loc_a101(
 	cid NVARCHAR(50),
 	cntry NVARCHAR(50),
 );
 GO
 
 ----------------------------------------------------------
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-	DROP TABLE bronze.erp_px_cat_g1v2;
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+	DROP TABLE silver.erp_px_cat_g1v2;
 GO
 
-CREATE TABLE bronze.erp_px_cat_g1v2(
+CREATE TABLE silver.erp_px_cat_g1v2(
 	id NVARCHAR(50),
 	cat NVARCHAR(50),
 	subcat NVARCHAR(50),
